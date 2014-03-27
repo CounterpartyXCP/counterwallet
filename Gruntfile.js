274facc6d628f48964d9df77327e5ec583a977d0
JahPowerBit <me@example.com>
2014-03-27 17:18:46 +0000

    add js concat and uglify in Grunfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,16 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         deploydir: 'dist/',
         useminPrepare: {
-            html: 'src/pages/index.html',
+            html: 'src/index.html',
             options: {
                 dest: 'dist',
                 root: 'src',
                 flow: {
                     html: {
-                        steps: {'css' : ['concat', 'cssmin']},
+                        steps: {
+                            'css' : ['concat', 'cssmin'], 
+                            'js': ['concat', 'uglifyjs']
+                        },
                         post: {}   
                     }                   
                 }
@@ -19,7 +22,7 @@ module.exports = function (grunt) {
         copy: {
             main: {
                 files: [
-                    {src: 'src/pages/index.html', dest: 'dist/index.html'},
+                    {src: 'src/index.html', dest: 'dist/index.html'},
                     {src: 'src/robots.txt', dest: 'dist/robots.txt'},
                     {src: 'src/pages/*', dest: 'dist/pages/'}
                 ]
@@ -30,33 +33,11 @@ module.exports = function (grunt) {
             html: 'dist/index.html'
         }
     });
-    grunt.loadNpmTasks('grunt-usemin')
-    grunt.loadNpmTasks('grunt-contrib-concat')
-    grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['useminPrepare', 'concat', 'cssmin', 'copy', 'usemin']);
+    grunt.registerTask('default', ['useminPrepare', 'concat', 'cssmin', 'uglify', 'copy', 'usemin']);
 };
-
-/*module.exports = function(grunt) {
-    // Grunt utilities
-    var config = grunt.config;
-    var utils = grunt.utils;
- 
-    // Deploy directory
-    var deploydir = '_deploy/';
- 
-    // Grunt configuration
-    config.init({
-        // final build output
-        deploydir: deploydir,
- 
-        processhtml: {
-            files: ['src/pages/*.html']
-        }
-    });
- 
-    grunt.loadTasks('./grunt-tasks/');
- 
-    grunt.registerTask('build', 'processhtml');
-};*/
