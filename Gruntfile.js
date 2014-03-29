2d324fc116e15a86607341cdc92cfaf7755819c4
JahPowerBit <me@example.com>
2014-03-29 11:56:18 +0000

    add writechecksum and checkdeps task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,11 @@
 module.exports = function (grunt) {
+
+    var Crypto = require('cryptojs').Crypto
+    var File = require('grunt-usemin/lib/file');
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        deploydir: 'dist/',
+
         useminPrepare: {
             html: 'src/index.html',
             options: {
@@ -10,11 +14,20 @@ module.exports = function (grunt) {
                 flow: {
                     html: {
                         steps: {
-                            'css' : ['concat', 'cssmin'], 
-                            'js': ['concat', 'uglifyjs']
+                            'css' : ['cssmin'], 
+                            'js': ['uglifyjs']
                         },
                         post: {}   
                     }                   
+                }
+            }
+        },
+
+        checksumdeps: {
+            root: 'src/',
+            html: {
+                'index.html': {
+                    "counterwallet-deps.js": true
                 }
             }
         },
@@ -33,11 +46,61 @@ module.exports = function (grunt) {
             html: 'dist/index.html'
         }
     });
+
     grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['useminPrepare', 'concat', 'cssmin', 'uglify', 'copy', 'usemin']);
+    var generateHashes = function() {
+        var hashes = {};
+        var config = grunt.config('checksumdeps');  
+        var root = "";
+        if (config.root) {
+            root = config.root;
+        }
+        for (var filename in config.html) {
+            var file = new File(root+filename);
+            file.blocks.forEach(function(block) {
+                blockdest = block.dest.split("/").pop();
+                if (config.html[filename][blockdest]) {
+                    for (var b in block.src) {
+                        grunt.log.writeln('blockfile:'+block.src[b]);
+                        var path = root+block.src[b]
+                        var content = grunt.file.read(path);
+                        var hash = Crypto.SHA256(content);
+                        hashes[path] = hash;
+                    }
+                }
+            });  
+        } 
+        return hashes;
+    }
+
+    grunt.registerTask('checkdeps', 'Check dependencies checksums', function() {
+        currenthashes = generateHashes();
+        savedhashes = grunt.file.readJSON("bowerchecksum.json");
+        
+        // We do not check if a file is missing
+        for (filename in currenthashes) {
+            currenthash = currenthashes[filename];
+            if (!savedhashes[filename]) {
+                grunt.fail.fatal("File not found in bowerchecksum.json: "+filename+". Please run 'grunt writechecksum'.");
+            } else if (savedhashes[filename]!=currenthash) {
+                grunt.fail.fatal("Invalid checksum: "+filename);
+            } else {
+                grunt.log.writeln("cheksum ok: "+filename);
+            }
+        }
+    });
+
+    grunt.registerTask('writechecksum', 'Generate dependencies checksums', function() {
+        hashesjs = generateHashes();
+        hashesjs = JSON.stringify(hashes, null, 4);
+        grunt.file.write("bowerchecksum.json", hashesjs);
+        grunt.log.writeln("generateHashes: "+hashesjs);
+    });
+
+    grunt.registerTask('default', ['checkdeps', 'useminPrepare', 'cssmin', 'uglify', 'copy', 'usemin']);
 };
