c4e262a8ed15d6ffa7e2efec032bae9b6e0a71c1
xnova <xnova@counterparty.co>
2014-04-06 22:17:19 +0000

    tagging v1.0.0 (yay!!!)

--- src/js/consts.js
+++ src/js/consts.js
@@ -2,7 +2,7 @@
 /***********
  * GLOBAL CONSTANTS
  ***********/
-var VERSION = "0.9.6 BETA";
+var VERSION = "1.0.0 BETA";
 
 var IS_MOBILE_OR_TABLET = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 var PREFERENCES = {}; //set when logging in
