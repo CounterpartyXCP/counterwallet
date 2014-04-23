3da9ff24b6d88292643618721f89be15333816bd
xnova <xnova@counterparty.co>
2014-04-23 06:18:27 +0000

    updated bowerhashes

--- src/js/util.api.js
+++ src/js/util.api.js
@@ -223,7 +223,7 @@ function _getDestTypeFromMethod(method) {
       'get_btc_address_info', 'get_btc_block_height', 'get_btc_txns_status',
       'get_num_users_online', 'get_chat_handle', 'store_chat_handle', 'get_preferences', 'store_preferences',
       'get_raw_transactions', 'get_balance_history',
-      'get_owned_assets', 'get_asset_history',
+      'get_owned_assets', 'get_asset_history', 'get_transaction_stats',
       'get_market_price_summary', 'get_market_price_history', 'get_market_info', 'get_market_info_leaderboard', 'get_market_cap_history',
       'get_order_book_simple', 'get_order_book_buysell', 'get_trade_history', 'get_trade_history_within_dates',
       'record_btc_open_order', 'cancel_btc_open_order'].indexOf(method) >= 0) {
