289182fd3bf075978a1e4c5f2d9a77ae7c3e6855
JahPowerBit <lightzarlboro@gmail.com>
2014-07-22 18:28:07 +0000

    add xbtc as a quotation asset

--- src/js/util.bitcoin.js
+++ src/js/util.bitcoin.js
@@ -51,16 +51,21 @@ function smartFormat(num, truncateDecimalPlacesAtMin, truncateDecimalPlacesTo) {
   return numberWithCommas(noExponents(num));
 }
 
+//TODO: refactor with a "currencies" list in conf
 function assetsToAssetPair(asset1, asset2) {
   //NOTE: This MUST use the same logic/rules as counterwalletd's assets_to_asset_pair() function in lib/util.py
   var base = null;
   var quote = null;
+  
   if(asset1 == 'BTC' || asset2 == 'BTC') {
       base = asset1 == 'BTC' ? asset2 : asset1;
       quote = asset1 == 'BTC' ? asset1 : asset2;
   } else if(asset1 == 'XCP' || asset2 == 'XCP') {
       base = asset1 == 'XCP' ? asset2 : asset1;
-      quote = asset1 == 'XCP' ? asset1 : asset2
+      quote = asset1 == 'XCP' ? asset1 : asset2;
+  } else if(asset1 == 'XBTC' || asset2 == 'XBTC') {
+      base = asset1 == 'XBTC' ? asset2 : asset1;
+      quote = asset1 == 'XBTC' ? asset1 : asset2;
   } else  {
       base = asset1 < asset2 ? asset1 : asset2;
       quote = asset1 < asset2 ? asset2 : asset1;
