45c6e5d9ce57a8b0f70831572f02c229fa5eafc5
xnova <xnova@counterparty.co>
2014-07-22 19:09:07 +0000

    fix removal of armory addresses

--- src/js/components/address.js
+++ src/js/components/address.js
@@ -192,8 +192,14 @@ function AddressViewModel(type, key, address, initialLabel, armoryPubKey) {
     WALLET.addresses.remove(self);
     
     //update the preferences with this address removed
-    var arr = PREFERENCES[self.TYPE == 'watch' ? 'watch_only_addresses' : 'armory_offline_addresses'];
-    arr = arrayRemove(arr, self.ADDRESS);
+    if(self.TYPE === 'watch') {
+      arrayRemove(PREFERENCES['watch_only_addresses'], self.ADDRESS);  
+    } else {
+      assert(self.TYPE === 'armory');
+      PREFERENCES['armory_offline_addresses'] = PREFERENCES['armory_offline_addresses'].filter(
+        function (el) { return el.address !== self.ADDRESS; });
+    }
+    
     WALLET.storePreferences(function() {
       checkURL(); //refresh the page without this address listed on it
     });
