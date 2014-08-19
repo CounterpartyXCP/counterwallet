1bba874066d9c8b6222b2c7ccb5abc0cbea847fe
xnova <xnova@counterparty.co>
2014-08-19 16:16:05 +0000

    pre4

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,7 +1,7 @@
 /***********
  * GLOBAL CONSTANTS
  ***********/
-var VERSION = "1.5.0PRE3 BETA";
+var VERSION = "1.5.0 PRE4 BETA";
 
 var IS_MOBILE_OR_TABLET = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 var PREFERENCES = {}; //set when logging in
