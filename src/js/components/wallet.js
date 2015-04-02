9924487433c0b1d5ae1397bc65d2866704573ae3
JahPowerBit <lightzarlboro@gmail.com>
2015-04-02 14:06:41 +0200

    [#688] Allow `OP_RETURN`; use`'auto'` encoding

--- src/js/components/wallet.js
+++ src/js/components/wallet.js
@@ -600,7 +600,7 @@ function WalletViewModel() {
     //specify the pubkey for a multisig tx
     assert(data['encoding'] === undefined);
     assert(data['pubkey'] === undefined);
-    data['encoding'] = 'multisig';
+    data['encoding'] = 'auto';
     data['pubkey'] = addressObj.PUBKEY;
     //find and specify the verifyDestAddr
 
